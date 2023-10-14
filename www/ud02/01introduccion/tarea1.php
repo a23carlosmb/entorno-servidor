27d15ffd4f5caf254dfcad7ec2f3017ed87c77a7
a23carlosmb <147239194+a23carlosmb@users.noreply.github.com>
2023-10-14 18:38:51 +0200

    Realizo tarea 1 ud2 intro

--- www/ud02/01introduccion/tarea1.php
+++ www/ud02/01introduccion/tarea1.php
@@ -1,10 +1,22 @@
 <?php
+// Tarea realizada por alumno Carlos Martínez Bourio
+
 /*
 Localiza y corrige los errores de este programa PHP:
-```php
-<? php
-echo '¿Cómo estás';
-echo 'Estoy bien, gracias.';
-??>
-```
+*/
+
+
+/* Eliminé la etiqueta '<? php' y el '?' que sobraba al final. 
+ * Cambié las comillas simples por dobles, aunque con comillas simples también funcionaba.
+ * y gregué un salto de línea por gusto personal.*/
+
+echo "¿Cómo estás?<br/>";
+echo "Estoy bien, gracias.";
+
+
+
+?>
+
+
+
 **/
