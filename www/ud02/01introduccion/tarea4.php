992994466dfb5f8f5ec0a5a61f5d3e2d62d4fa08
a23carlosmb <147239194+a23carlosmb@users.noreply.github.com>
2023-10-18 13:39:36 +0200

    Realizo tarea4.php

--- www/ud02/01introduccion/tarea4.php
+++ www/ud02/01introduccion/tarea4.php
@@ -1,7 +1,10 @@
 <?php
 
+// Tarea realizada por alumno Martínez Bourio
+
 /*Haz una página que ejecute la función phpinfo() y que muestre las principales variables de servidor mencionadas en teoría.
 */
 
+phpinfo();
 
 ?>
