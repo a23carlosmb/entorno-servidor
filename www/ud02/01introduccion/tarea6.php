7f809acc5fee69fb7a4645a95f832fe223568934
a23carlosmb <147239194+a23carlosmb@users.noreply.github.com>
2023-10-18 15:49:26 +0200

    Realizo tarea6.php

--- www/ud02/01introduccion/tarea6.php
+++ www/ud02/01introduccion/tarea6.php
@@ -1,7 +1,18 @@
 <?php
 
+// Tarea realizada por alumno Martínez Bourio.
+
+$nombre = "Charlie";
+
 //Usando la instrucción ```echo``` crea un programa en PHP que muestre el mensaje: ```Hola, Mundo!```  Optativo. Muéstralo en cursiva.
 
+echo "<p><i>Hola, Mundo!</i></p><hr/>";
+
 //Crea un programa en PHP que guarde en una variable tu nombre y lo muestre en negrita formando el siguiente mensaje: Bienvenido tunombre
+
+echo "<p>VARIABLE:</p>";
+echo '<strong>$nombre: "Charlie";</strong>';
+echo "<p>RESULTADO:</P>";
+echo "<p>Bienvenido $nombre</p>";
 
 ?>
