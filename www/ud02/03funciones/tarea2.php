56958cb1c001cd354190cd750cee76830767047b
a23carlosmb <147239194+a23carlosmb@users.noreply.github.com>
2023-10-25 15:27:04 +0200

    Realizo tarea2.php

--- www/ud02/03funciones/tarea2.php
+++ www/ud02/03funciones/tarea2.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Crea unha función chamada `comprobar_nif()` que reciba un NIF e devolva:
  *  `true` se o NIF é correcto.
@@ -8,5 +8,54 @@
  * Dividimos o número entre 23.
  * Co resto da división anterior, obtemos a letra corresponde na seguinte táboa: 
  */
+
+// Creo una funcion para comprar si el NIF es válido:
+function comprobar_NIF($NIF)
+{
+    // Comprobar si el NIF tiene un formato válido (8 dígitos seguidos de una letra).
+    if (preg_match('/^\d{8}[a-zA-Z]$/', $NIF)) {
+        // Extraer el número (los 8 primeros dígitos) y la letra (último carácter).
+        $numero = substr($NIF, 0, 8);
+        $letra = strtoupper(substr($NIF, -1)); // Asegurarse de que la letra esté en mayúsculas.
+
+        // Calcular la letra correspondiente al número del NIF.
+        $letras_validas = "TRWAGMYFPDXBNJZSQVHLCKE";
+        $indice = $numero % 23;
+
+        // Comprobar si la letra del NIF coincide con la calculada.
+        if ($letra === $letras_validas[$indice]) {
+            return true; // El NIF es válido.
+        }
+    }
+
+    return false; // El NIF no es válido.
+
+}
+
+// A mayores, creo otra función para que imprima en pantalla el resultado de la comprobación, porque no supe hacerlo todo en una:
+
+function resultado_comprobar_NIF($NIF)
+{
+    if (comprobar_nif($NIF)) {
+        echo "El NIF es <span style='color:green;'>válido.</span>";
+    } else {
+        echo "El NIF es <span style='color:red;'>inválido.</span>";
+    }
+}
+
+// Pruebo si funciona:
+
+$NIF = "12345678Z";
+
+
+echo "<p>Comprobar NIF, prueba:</p>";
+echo "<p>\$NIF = $NIF</p>";
+
+resultado_comprobar_NIF($NIF); 
+
+$NIF = "Y7612227T";
+echo "<p>\$NIF= $NIF</p>";
+resultado_comprobar_NIF($NIF); 
+
 
 ?>
