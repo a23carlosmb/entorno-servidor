ddc81f1315c224f02b7060ddf2bca68792d3b2e1
a23carlosmb <147239194+a23carlosmb@users.noreply.github.com>
2023-10-25 13:16:09 +0200

    Realizo tarea3.php

--- www/ud02/02arrays/tarea3.php
+++ www/ud02/02arrays/tarea3.php
@@ -6,6 +6,22 @@ Uso de la función [rand](https://www.php.net/manual/es/function.rand.php).
 Imprima la matriz creada anteriormente.
 */ 
 
+
+$matriz = array();
+
+// Creo un bucle para mostrar los números aleatorios con rand
+for ($i = 0; $i < 30; $i++) {
+    $numeroAleatorio = rand(0, 20);
+    $matriz[] = $numeroAleatorio;
+}
+// LOs imprimo, con otro foreah
+
+foreach ($matriz as $numero) {
+    echo $numero . '<br/> ';
+}
+
+// Prometo hacer los optativos para practicar, no hoy pero sí, los haré
+
 /* 
 2. (Optativo) Cree una matriz con los siguientes datos: 
 `Batman`, `Superman`, `Krusty`, `Bob`, `Mel` y `Barney`.
