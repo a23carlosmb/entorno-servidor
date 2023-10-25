568511f060125924770489dc80745a8ec5e4e63e
a23carlosmb <147239194+a23carlosmb@users.noreply.github.com>
2023-10-25 13:10:30 +0200

    Realizo tarea2.php

--- www/ud02/02arrays/tarea2.php
+++ www/ud02/02arrays/tarea2.php
@@ -1,5 +1,7 @@
 <?php 
 
+// Tarea realizada por Martínez Bourio
+
 /*
 Necesitamos almacenar la siguiente información en un array multidimensional:
 
@@ -27,5 +29,40 @@ Necesitamos almacenar la siguiente información en un array multidimensional:
 Almacena e imprime la información. 
 */
 
+$informacion = array(
+    'John' => array(
+        'email' => 'john@demo.com',
+        'website' => 'www.john.com',
+        'age' => 22,
+        'password' => 'pass'
+    ),
+    'Anna' => array(
+        'email' => 'anna@demo.com',
+        'website' => 'www.anna.com',
+        'age' => 24,
+        'password' => 'pass'
+    ),
+    'Peter' => array(
+        'email' => 'peter@mail.com',
+        'website' => 'www.peter.com',
+        'age' => 42,
+        'password' => 'pass'
+    ),
+    'Max' => array(
+        'email' => 'max@mail.com',
+        'website' => 'www.max.com',
+        'age' => 33,
+        'password' => 'pass'
+    )
+);
 
+// Muestro con un foreach:
+foreach ($informacion as $nombre => $datos) {
+    echo "Nombre: $nombre<br>";
+    echo "Email: " . $datos['email'] . "<br>";
+    echo "Website: " . $datos['website'] . "<br>";
+    echo "Edad: " . $datos['age'] . "<br>";
+    echo "Contraseña: " . $datos['password'] . "<br><br>";
+}
 ?>
+
